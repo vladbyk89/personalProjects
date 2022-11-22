072ad03311abb58e6d7aa199fbb613c99595f552
vladb89 <vbykanov1989@outlook.com>
2022-11-22 13:06:07 +0100

    to do list first commit

---
+++ toDoList/app.js
@@ -0,0 +1,56 @@
+const ulEl = document.getElementById('ul-el')
+const inputField = document.getElementById('input-field')
+const clearBtn = document.getElementById('clear-btn')
+
+let myTasks = []
+
+
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myTasks'))
+
+if(leadsFromLocalStorage){
+    myTasks = leadsFromLocalStorage;
+    renderArr(myTasks)
+}
+
+
+// main function to save tasks
+inputField.addEventListener('keypress', function(e){
+    if (e.key == 'Enter'){
+        console.log('Enter pressed')
+        let inputValue = inputField.value
+        inputField.value = ' '
+        myTasks.push(inputValue)
+        console.log(myTasks)
+        localStorage.setItem("myTasks", JSON.stringify(myTasks));
+        renderTask(inputValue)
+    }
+})
+
+function renderArr(tasks){
+    if (myTasks.length == 0){
+        ulEl.replaceChildren()
+    }
+    for (let i = 0; i < tasks.length; i++)
+    {
+        renderTask(tasks[i])
+    }
+}
+
+function renderTask(task){
+    let liEl = document.createElement('li')
+    liEl.textContent = task
+    let btn = document.createElement('button')
+    liEl.setAttribute('id', myTasks.length)
+    btn.addEventListener("click", () => liEl.classList.add("delete"))
+    btn.textContent = 'X'
+    liEl.appendChild(btn)
+    ulEl.append(liEl)
+    console.log(myTasks)
+}
+
+clearBtn.addEventListener('click', function(){
+    localStorage.clear()
+    myTasks = []
+    renderArr(myTasks)
+})
---
+++ toDoList/dist/app.dev.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var ulEl = document.getElementById('ul-el');
+var inputField = document.getElementById('input-field');
+var clearBtn = document.getElementById('clear-btn');
+var myTasks = [];
+var leadsFromLocalStorage = JSON.parse(localStorage.getItem('myTasks'));
+
+if (leadsFromLocalStorage) {
+  myTasks = leadsFromLocalStorage;
+  renderArr(myTasks);
+} // main function to save tasks
+
+
+inputField.addEventListener('keypress', function (e) {
+  if (e.key == 'Enter') {
+    console.log('Enter pressed');
+    var inputValue = inputField.value;
+    inputField.value = ' ';
+    myTasks.push(inputValue);
+    console.log(myTasks);
+    localStorage.setItem("myTasks", JSON.stringify(myTasks));
+    renderTask(inputValue);
+  }
+});
+
+function renderArr(tasks) {
+  if (myTasks.length == 0) {
+    ulEl.replaceChildren();
+  }
+
+  for (var i = 0; i < tasks.length; i++) {
+    renderTask(tasks[i]);
+  }
+}
+
+function renderTask(task) {
+  var liEl = document.createElement('li');
+  liEl.textContent = task;
+  var btn = document.createElement('button');
+  liEl.setAttribute('id', myTasks.length);
+  btn.addEventListener("click", function () {
+    return liEl.classList.add("delete");
+  });
+  btn.textContent = 'X';
+  liEl.appendChild(btn);
+  ulEl.append(liEl);
+  console.log(myTasks);
+}
+
+clearBtn.addEventListener('click', function () {
+  localStorage.clear();
+  myTasks = [];
+  renderArr(myTasks);
+});
